67c8a873194a46d8eff3af61c7ecca26e9aa75c2
thaihoan2703 <hoanthai1103@gmail.com>
2023-04-26 21:25:55 +0700

    v

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "./Page/Home";
+import MotorPage from "./Page/Motor";
+import ContactPage from "./Page/Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/motor">Motor</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/motor" element={<MotorPage />}></Route>
+        <Route path="/contact" element={<ContactPage />}></Route>
+      </Routes>
     </div>
   );
 }
